cb19ae4d4326269a91b788705d7256a9c4ce4815
ttpro1995 <ttpro1995@yahoo.com.vn>
2020-01-28 22:24:50 +0700

    make it responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,15 +11,18 @@ import { AnothertopbarComponent } from './anothertopbar/anothertopbar.component'
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
 
+
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'gallery', component: GalleryComponent },
 ];
 
+
 @NgModule({
   imports:      [ 
     BrowserModule,
     FormsModule,
+    NgbModule, // bootstrap
     RouterModule.forRoot(appRoutes)  ],
   declarations: [ AppComponent, HelloComponent, TopBarComponent, AnothertopbarComponent, HomeComponent, GalleryComponent ],
   bootstrap:    [ AppComponent ]
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-
+  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  
+  image;
+  
   constructor() { }
 
   ngOnInit() {
+    this.image = "http://placekitten.com/g/500/600"; // placeholder
   }
 
 }
