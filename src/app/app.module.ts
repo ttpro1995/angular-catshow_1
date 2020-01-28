8f16d766ec8f8a8bf1224520b956a156b3b36fb0
ttpro1995 <ttpro1995@yahoo.com.vn>
2020-01-28 23:20:58 +0700

    the cat api work, but so broken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { AnothertopbarComponent } from './anothertopbar/anothertopbar.component';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { CatApiService } from './cat-api.service';
 
 
 const appRoutes: Routes = [
@@ -21,10 +24,12 @@ const appRoutes: Routes = [
 @NgModule({
   imports:      [ 
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     NgbModule, // bootstrap
     RouterModule.forRoot(appRoutes)  ],
   declarations: [ AppComponent, HelloComponent, TopBarComponent, AnothertopbarComponent, HomeComponent, GalleryComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [CatApiService]
 })
 export class AppModule { }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CatApiService } from '../cat-api.service';
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -9,11 +11,21 @@ export class GalleryComponent implements OnInit {
   // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   
   image;
-  
-  constructor() { }
+  fulljson;
+  fulljsonStr;
+
+  constructor(
+    private catapi: CatApiService
+    ) { }
 
   ngOnInit() {
     this.image = "http://placekitten.com/g/500/600"; // placeholder
+  }
+
+  requestData() {
+    this.fulljson = this.catapi.getRandomCat();
+    this.fulljsonStr = JSON.stringify(this.fulljson);
+    this.image = this.fulljson[0].url;
   }
 
 }
